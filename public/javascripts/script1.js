a5ff2012f6033c379f3a365af8285a855e79cb8d
Jordan Eggers <jordaneggers@Jordans-MacBook-Pro.local>
2022-02-25 17:03:08 -0500

    get contact ids to show on page

--- public/javascripts/script1.js
+++ public/javascripts/script1.js
@@ -1,13 +1,76 @@
 class Model {
+  constructor() {
+    this.contacts
+    this.getContacts();
+  }
 
+  async getContacts () {
+    let contacts = await fetch('api/contacts', {
+      method: 'GET'
+    }).then(contacts => contacts.json())
+      .catch(() => alert('can\'t get contacts'));
+    console.log(contacts);
+    return contacts;
+    //this.contacts = await response.json();
+    //console.log(this.contacts); //test
+  }
 }
 
 class View {
+  constructor() {
+    this.app = this.getElement('#root');
+    this.contactsList = this.createElement("ul", "contacts-list");
+  }
 
+  createElement(tag, className) {
+    let element = document.createElement(tag);
+    if (className) element.classList.add(className);
+    return element;
+  }
+
+  getElement(selector) {
+    let element = document.querySelector(selector);
+    return element;
+  }
+
+  async displayContacts (contacts) {
+    //Delete any existing nodes
+    // while (this.contactsList.firstChild) {
+    //  this.contactsList.removeChild(this.contactsList.firstChild);
+    // }
+    if (contacts.length === 0) {
+      let p = this.createElement('p');
+      p.textContent = "There are no contacts.";
+      this.contactsList.append(p);
+    } else {
+      //add contact nodes
+      contacts.forEach(contact => {
+        let li = this.createElement('li');
+        li.textContent = `${String(contact.id)}`;
+        this.contactsList.append(li);
+      });
+    }
+    this.app.append(this.contactsList);
+  }
 }
 
 class Controller {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
 
+    //binding stuff needs to go here
+    //Display initial todos
+    this.addContactsToPage();
+  }
+
+
+
+  async addContactsToPage() {
+    let contacts = await this.model.getContacts();
+    console.log(contacts.length);
+    this.view.displayContacts(contacts);
+  }
 }
 
 class App {
