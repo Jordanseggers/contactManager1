ba01d8ffe008893d298dbe964f55cc6bf61f0226
Jordan Eggers <jordaneggers@Jordans-MacBook-Pro.local>
2022-03-01 10:48:54 -0500

    tags added to display, handlebars using each

--- public/javascripts/script2.js
+++ public/javascripts/script2.js
@@ -1,9 +1,12 @@
 let addContactButton1 = getElement(".btn-add-contact");
 let app = getElement("#root");
+let search = getElement(".contact-name-search");
+let contacts;
 let form;
+let tagOptions = ['work', 'school'];
 
 async function getContacts () {
-  let contacts = await fetch('api/contacts', {
+  contacts = await fetch('api/contacts', {
     method: 'GET'
   }).then(res => {
     return res.json()
@@ -27,51 +30,73 @@ function getElement(selector) {
   return element;
 }
 
+function convertTags (contacts) {
+  let contactsArr = Array.prototype.slice.call(contacts);
+  contactsArr.forEach(contact => {
+    if(contact.tags) {
+      contact["tags"] = contact["tags"].split(',');
+    }
+  });
+  return contactsArr;
+}
+
 async function displayContacts () {
   let contactsList = createElement("ul", "contacts-list");
-  let contacts = await getContacts();
+  contacts = await getContacts();
   let contactHtml = getElement("#contactCard").innerHTML;
   let contactTemplate = Handlebars.compile(contactHtml);
 
-  // while(contactsList.firstChild) {
-  //   contactsList.removeChild(contactsList.firstChild);
-  // }
-
-  if(contactsList.length === 0) {
+  if(contacts.length === 0) {
+    console.log('apparently, the contacts length is 0')
     let noContacts = createElement('p');
     noContacts.textContent = "There are no contacts.";
     contactsList.append(noContacts);
   } else {
-    contacts.forEach(contact => {
-      let li = createElement('li', 'media col-md-3 col-sm-4');
-      li.innerHTML = contactTemplate(contact);
-      contactsList.append(li);
-    });
-    app.innerHTML = '';
-    app.append(contactsList);
+    contacts = convertTags(contacts);
+    contactsList.innerHTML = contactTemplate({contacts:contacts});
   }
+
+  app.innerHTML = '';
+  app.append(contactsList);
 
   contactsList.addEventListener('click', (event) => {
     event.preventDefault();
     if(event.target.classList.contains("delete-contact")) {
       deleteContact(event.target.parentNode);
+    } else if (event.target.classList.contains("edit-contact")) {
+      let contactId = event.target.parentNode.dataset.contactId;
+      contactForm(Number(contactId));
     }
   });
 }
 
-function addContactForm () {
+function contactForm (existingId) {
   let formHTML = getElement("#new-contact-form").innerHTML;
   let formTemplate = Handlebars.compile(formHTML);
 
   let div = createElement('div');
   div.innerHTML = formTemplate({});
+  if (typeof existingId === "number") {
+    let contact = contacts.filter(contact => contact.id === Number(existingId))[0];
+    let nameField = div.querySelector('.contact-name-input');
+    let emailField = div.querySelector('.contact-email-input');
+    let phoneField = div.querySelector('.contact-phone-input');
+    nameField.setAttribute("placeholder", `${contact["full_name"]}`);
+    emailField.setAttribute("placeholder", `${contact["email"]}`);
+    phoneField.setAttribute("placeholder", `${contact["phone_number"]}`);
+  }
   
   app.innerHTML = '';
   app.append(div);
 
   form = getElement('form');
 
-  form.addEventListener('submit', addContact);
+  if(typeof existingId === "number") {
+    form.dataset.contactId = existingId;
+    form.addEventListener('submit', editContact);
+  } else {
+    form.addEventListener('submit', addContact);
+  }
 }
 
 function formDataToJSON(formData) {
@@ -109,9 +134,85 @@ async function deleteContact (element) {
   displayContacts();
 }
 
+function sanitizeObject (object) {
+  let keys = Object.keys(object);
+  let newObj = {};
+  keys.forEach(key => {
+    if(object[key] !== '') {
+      newObj[key] = object[key];
+    }
+  });
+  return newObj;
+}
+
+async function editContact (event) {
+  event.preventDefault();
+  let contactId = event.target.dataset.contactId;
+  
+  let formData = new FormData(event.target);
+  let jsonData = formDataToJSON(formData);
+  let usefulFormData = sanitizeObject(jsonData);
+  usefulFormData.id = Number(contactId);
+
+  await fetch(`/api/contacts/${contactId}`, {
+    method: 'PUT',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(usefulFormData)
+  }).then(res => console.log(res.body))
+    .catch(console.log('failed'));
+
+  displayContacts();
+}
+
+function filterContacts() {
+  let value = search.value;
+  let cards = document.querySelectorAll(".card");
+  let cardsArr = Array.prototype.slice.call(cards);
+  cardsArr.forEach(card => {
+    if (value === "") {
+      console.log(`unhide all`);
+      card.classList.toggle('hide', false);
+    } else {
+      let name = card.querySelector('h3').textContent;
+      let includes = value;
+      console.log(includes);
+      console.log(new RegExp(includes, 'i'));
+      if (!name.match(new RegExp(includes))) {
+        console.log(`hiding card ${name}`);
+        card.classList.toggle('hide', true);
+      }
+    }
+  })
+}
+
+// function tagForm () {
+//   let formHTML = getElement("#new-tag").innerHTML;
+//   let formTemplate = Handlebars.compile(formHTML);
+
+//   let div = createElement('div');
+//   div.innerHTML = formTemplate({});
+
+//   let currentTags = div.querySelector('.current-tags');
+//   tagOptions.forEach(tag => {
+//     let dd = createElement('dd');
+//     dd.textContent = tag;
+//     currentTags.append(dd);
+//   });
+
+//   app.innerHTML = '';
+//   app.append(div);
+
+//   form = getElement('form');
+//   form.addEventListener('submit', (event) => {
+//     let newTag = event.target.tag.value;
+//     console.log(newTag);
+//   })
+// }
+
 document.addEventListener('DOMContentLoaded', () => {
   displayContacts();
 });
 
-addContactButton1.addEventListener('click', addContactForm);
+addContactButton1.addEventListener('click', contactForm);
+search.addEventListener('input', filterContacts);
 
